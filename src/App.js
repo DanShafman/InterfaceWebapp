264b47b1e038c8dc8bc0c9e1507f847b035e9873
DanShafman <dshafman0900@bths.edu>
2019-06-27 05:10:43 -0400

    Got wireframes 2(?) and 3 working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
-import React from 'react';
-import { Text, Switch, View, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { Text, Switch, View, StyleSheet, TouchableOpacity } from "react-native";
 import { stylesheet } from './styles.js'
+
 
 const styles = StyleSheet.create(stylesheet);
 
-
-
-function App() {
-  console.log(stylesheet);
-  return (
-    <div>
-      <video src={require("./data/9clip.mp4")} width={document.documentElement.clientWidth} controls></video>
-      <View style={styles.descriptionBox}>
-        <Text>Suggested Sound-sources</Text>
-      </View>
-      <View style={styles.switchCase}>
-        <Switch style={styles.switch}></Switch>
-      </View>
-      
-    </div>
-    // <View>
-    //   <Text>henlo</Text>
-    // </View>
-  );
+class VideoPlayer extends Component {
+  render() {
+    return (
+      <video src={require("./data/" + this.props.children)} width={document.documentElement.clientWidth} controls></video>
+    )
+  }
 }
 
-export default App;
+class SwitchContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {switchValue: false};
+    this.handleSwitchToggled = this.handleSwitchToggled.bind(this);
+  }
+
+  handleSwitchToggled() {
+    this.setState({switchValue: !this.state.switchValue});
+  }
+
+  render() {
+    return(
+      <View style={styles.switchCase}>
+        <Text style={styles.switchDescription}>{this.props.children}</Text>
+        <Switch style={styles.switch} onValueChange={this.handleSwitchToggled} value={this.state.switchValue}></Switch>
+      </View>
+    )
+  }
+}
+
+class ConfirmAnnotationsButton extends Component {
+  handleConfirmAnnotationsAction() {
+  }
+
+  render() {
+    return(
+      <View style={{justifyContent: 'center'}}>
+        <TouchableOpacity style={styles.confirmButton} title="Confirm" onClick={this.handleConfirmAnnotationsAction}>
+          <Text style={styles.confirmButtonText}>Confirm</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+export default class AppConditionTwo extends Component {
+  render() {
+    return (
+      <div>
+        <VideoPlayer>2clip.mp4</VideoPlayer>
+
+        <View style={styles.descriptionBoxContainer}>
+          <Text style={styles.titleBox}>Suggested Sound-sources</Text>
+          <Text style={{fontFamily: 'sans-serif-light',}}>Switch on confirms the sound-source is present. 
+Switch off if the sound is not present in the clip.
+Click on the label for further details about the label.</Text>
+        </View>
+
+        <SwitchContainer>Jackhammer</SwitchContainer>
+        <SwitchContainer>Rock Drill</SwitchContainer>
+        <SwitchContainer>Saw</SwitchContainer>
+        <SwitchContainer>Small Engine</SwitchContainer>
+        <SwitchContainer>Large Engine</SwitchContainer>
+        <SwitchContainer>Human Voice</SwitchContainer>
+        <SwitchContainer>Dog Barking</SwitchContainer>
+        <SwitchContainer>Sensor Malfunction</SwitchContainer>
+        <SwitchContainer>Hammer</SwitchContainer>
+        <SwitchContainer>Impact Machinery</SwitchContainer>
+        <SwitchContainer>Car Horn</SwitchContainer>
+        <SwitchContainer>Reverse Beep</SwitchContainer>
+        <SwitchContainer>Brakes</SwitchContainer>
+
+        <ConfirmAnnotationsButton/>
+      </div>
+    );
+  }
+}
--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,17 @@
 export const stylesheet = {
-    descriptionBox: {
+    descriptionBoxContainer: {
       marginTop: 10,
       marginBottom: 10,
+      marginLeft: 20,
+      marginRight: 20,
       alignItems: 'center'
+    },
+    
+    titleBox: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      fontFamily: 'sans-serif-light',
+      marginBottom: 10
     },
   
     switch: {
@@ -10,12 +19,42 @@ export const stylesheet = {
       marginTop: 10,
       marginRight: 10,
       marginBottom: 10,
+      maxWidth: 40,
       alignSelf: 'flex-end'
+    },
+  
+    switchDescription: {
+      flex: 1,
+      alignSelf: 'flex-start',
+      fontFamily: 'sans-serif-light',
+      marginLeft: 10,
+      marginTop: 10
+
     },
   
     switchCase: {
       borderBottomWidth: 1,
+      // borderWidth: 1,
       borderColor: 'gainsboro',
+      flexDirection: 'row'
+    },
+
+    // confirmButtonCase: {
+    //   height: 50
+    // },
+
+    confirmButton: {
+      height: 50,
+      alignItems: 'center',
+      backgroundColor: 'green',
+      padding: 15,
+      // borderTopWidth: 1
+    },
+
+    confirmButtonText: {
+      fontSize: 20,
+      fontFamily: 'sans-serif-light',
+      color: 'white'
     }
   
 };
